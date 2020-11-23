f664e77db8a0051e6044337a07a4a2629062ffe8
larutow <lucas.rutowski@gmail.com>
2020-11-23 11:22:54 -0600

    added index 0 for main menu name prompt

--- most-wanted-code/app.js
+++ most-wanted-code/app.js
@@ -33,7 +33,7 @@ function mainMenu(person, people){
     return app(people); // restart
   }
 
-  let displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
+  let displayOption = prompt("Found " + person[0].firstName + " " + person[0].lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
 
   switch(displayOption){
     case "info":
