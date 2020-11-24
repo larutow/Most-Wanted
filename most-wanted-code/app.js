70b4974690a26509ea89e84de7ea80c4840e39ac
larutow <lucas.rutowski@gmail.com>
2020-11-23 20:23:43 -0600

    fixed lowercase trait search, added person display logic

--- most-wanted-code/app.js
+++ most-wanted-code/app.js
@@ -76,7 +76,7 @@ function searchByTrait(people){
   let foundPeople = "";
   
   let traitType = promptFor("Choose trait: Occupation, Height, Weight, Gender, Eye Color", chars);
-  traitType.toLowerCase();
+  traitType = String.toLowerCase(traitType);
   switch (traitType){
     case "occupation":
       let chosenOccupation = promptFor("Choose occupation type", chars);
@@ -166,6 +166,14 @@ function displayPerson(person){
   // height, weight, age, name, occupation, eye color.
   let personInfo = "First Name: " + person.firstName + "\n";
   personInfo += "Last Name: " + person.lastName + "\n";
+  personInfo += "Gender: " + person.gender + "\n";
+  personInfo += "DOB: " + person.dob + "\n";
+  personInfo += "Height: " + person.height + "\n";
+  personInfo += "Weight: " + person.weight + "\n";
+  personInfo += "Eye Color: " + person.eyeColor + "\n";
+  personInfo += "Occupation: " + person.occupation + "\n";
+
+
   // TODO: finish getting the rest of the information to display
   alert(personInfo);
 }
